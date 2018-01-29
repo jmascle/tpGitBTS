53c81a7904f88cc9b263b4f7530290240296375a
jmascle <jules.mascle@hotmail.fr>
2018-01-29 17:20:26 +0100

    ajouter le fichier

--- calculatrice/src/calculatrice/aug.java
+++ calculatrice/src/calculatrice/aug.java
@@ -2,5 +2,4 @@ package calculatrice;
 
 public class aug {
 	public static void Main(String[] args) {}
-
 }
---
+++ calculatrice/src/calculatrice/jul.java
@@ -0,0 +1,10 @@
+package calculatrice;
+
+public class jul {
+	public static void Main(String[] args) {}
+scanner sc = new Scanner(Systeme.in)
+Systeme.out.println("Veuillez choisir le type de calcul à faire : 1= + ; 2 = - ; 3 = / ; 4 = +" )
+}
+
+
+}
